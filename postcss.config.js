aad7e9ba6989f243801bb96be29fdedb2f9e9cb6
Shuvajyoti Bardhan <shuvajyotibardhan@Shuvajyotis-MacBook-Air.local>
2025-10-08 12:29:49 -0600

    FINAL FILE FIX: Aggressively removing old postcss.config.js to force load of postcss.config.cjs.

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-// This standalone file explicitly defines the PostCSS plugins.
-// It uses the robust CommonJS 'require' pattern which is the most reliable 
-// way to load plugins in mixed or stubborn Node environments.
-module.exports = {
-  plugins: {
-    'tailwindcss': {},
-    'autoprefixer': {},
-  },
-}
